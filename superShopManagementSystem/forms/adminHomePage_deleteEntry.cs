c09b0269a0a916b1ea4e51d0b979f03744b1ec39
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2022-05-11 04:57:19 +0600

    updated adminPage_deleteEntry with try catch block

--- superShopManagementSystem/forms/adminHomePage_deleteEntry.cs
+++ superShopManagementSystem/forms/adminHomePage_deleteEntry.cs
@@ -21,45 +21,37 @@ namespace superShopManagementSystem.forms
 
         private void DeleteManagerButton_Click(object sender, EventArgs e)
         {
-            try
+
+            Connection CN = new Connection();
+            if (radioButtonclassBase.optionRadio == ENUMsalesManORmanager.salesMan)
             {
-                Connection CN = new Connection();
-                if (radioButtonclassBase.optionRadio == ENUMsalesManORmanager.salesMan)
-                {
-                    //title
-                    label1.Text = "AdminPage: salesMan New Entry";
-                    //querrySalesMan
-                    sp_delete = "Delete from salesman where ID= '" + this.DeleteIdBoxManager.Text + "'";
-                }
-                else
-                {
-                    //title
-                    label1.Text = "AdminPage: inventoryManager New Entry";
-                    //querry inventory_manager
-                    sp_delete = "Delete from inventory_login where ID= '" + this.DeleteIdBoxManager.Text + "'";
-                }
-
-                CN.thisConnection.Open();
-                SqlCommand cmd = new SqlCommand(sp_delete, CN.thisConnection);
-
-
-                int i = cmd.ExecuteNonQuery();
-
-                CN.thisConnection.Close();
-                if (i < 1)
-                {
-                    ERRORLABEL.Text = "data not found";
-                }
-
-                CN.thisConnection.Close();
-
+                //title
+                label1.Text = "AdminPage: salesMan New Entry";
+                //querrySalesMan
+                sp_delete = "Delete from salesman where ID= '" + this.DeleteIdBoxManager.Text + "'";
             }
-            catch (Exception ex)
+            else
             {
-                ERRORLABEL.Text = ex.ToString();
+                //title
+                label1.Text = "AdminPage: inventoryManager New Entry";
+                //querry inventory_manager
+                sp_delete = "Delete from inventory_login where ID= '" + this.DeleteIdBoxManager.Text + "'";
             }
-            
-             
+
+            CN.thisConnection.Open();
+            SqlCommand cmd = new SqlCommand(sp_delete, CN.thisConnection);
+
+
+            int i = cmd.ExecuteNonQuery();
+
+            CN.thisConnection.Close();
+            if (i < 1)
+            {
+                ERRORLABEL.Text = "data not found";
+            }
+
+            CN.thisConnection.Close();
+
         }
     }
 }
